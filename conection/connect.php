edf2b4d7d2b5bb02ed6e7126d3c4a89ebb5062d4
Jazib Bashir <jb@teknuk.com>
2015-04-19 11:05:50 +0500

    Updating Script

--- conection/connect.php
+++ conection/connect.php
@@ -1,4 +1,4 @@
 <?php
-mysql_connect("localhost","root","") or die("No Connection");
-mysql_select_db("assighment") or die("No Database name");
+	mysql_connect("localhost","root","") or die("No Connection");
+	mysql_select_db("assighment") or die("No Database name");
 ?>
