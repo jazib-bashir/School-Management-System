591f571b004452f872ae979059afa16c46d46a8e
saif-ur-rehman <saif54624@yahoo.com>
2015-04-19 12:51:24 +0300

    DB Name Change

--- conection/connect.php
+++ conection/connect.php
@@ -1,4 +1,4 @@
 <?php
 	mysql_connect("localhost","root","") or die("No Connection");
-	mysql_select_db("assighment") or die("No Database name");
+	mysql_select_db("school_project") or die("No Database name");
 ?>
