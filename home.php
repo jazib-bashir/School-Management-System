a5e2e48b7ac36370b09ef18acfd07d93fb3080a9
Jazib  Bashir <jazibbashir@gmail.com>
2015-06-06 20:57:46 +0500

    slider added in home page

--- home.php
+++ home.php
@@ -1,6 +1,60 @@
+    <div class="wrapper">
 
-<div class="col-md-12">
-	<img src="picture/master.jpg" class="img-responsive">
-	<h4>What do you know about School?</h4>
+    <ul id="sb-slider" class="sb-slider">
+        <li>
+           <img src="images/1.jpg" alt="image1"/>
+            <div class="sb-description">
+                <h3>Creative Lifesaver</h3>
+            </div>
+        </li>
+        <li>
+            <img src="images/2.jpg" alt="image2"/>
+            <div class="sb-description">
+                <h3>Honest Entertainer</h3>
+            </div>
+        </li>
+        <li>
+            <img src="images/3.jpg" alt="image1"/>
+            <div class="sb-description">
+                <h3>Brave Astronaut</h3>
+            </div>
+        </li>
+        <li>
+            <img src="images/4.jpg" alt="image1"/>
+            <div class="sb-description">
+                <h3>Affectionate Decision Maker</h3>
+            </div>
+        </li>
+        <li>
+            <img src="images/5.jpg" alt="image1"/>
+            <div class="sb-description">
+                <h3>Faithful Investor</h3>
+            </div>
+        </li>
+        <li>
+            <img src="images/6.jpg" alt="image1"/>
+            <div class="sb-description">
+                <h3>Groundbreaking Artist</h3>
+            </div>
+          </li>
+        <li>
+           <img src="images/7.jpg" alt="image1"/>
+            <div class="sb-description">
+                <h3>Selfless Philantropist</h3>
+            </div>
+        </li>
+    </ul>
+
+    <div id="nav-arrows" class="nav-arrows">
+        <a href="#">Next</a>
+        <a href="#">Previous</a>
+    </div>
+
+</div>
+    <div class="col-md-12 wrapper">
+
+    <div class="white-color">
+    <h4>What do you know about School?</h4>
 	<p>Stylized implementation of HTML's <abbr> element for abbreviations and acronyms to show the expanded version on hover. Abbreviations with a title attribute have a light dotted bottom border and a help cursor on hover, providing additional context on hover and to users of assistive technologies.</p>
+    </div>
 </div>
