5f7b22d91bab3adba3c92529cb5c1577af0562b5
Jazib  Bashir <jazibbashir@gmail.com>
2015-06-06 20:14:59 +0500

    live hosting 000webshost

--- conection/connect.php
+++ conection/connect.php
@@ -2,3 +2,9 @@
 	mysql_connect("localhost","root","") or die("No Connection");
 	mysql_select_db("school_project") or die("No Database name");
 ?>
+
+
+<!-- $mysql_host = "mysql2.000webhost.com";
+$mysql_database = "a4451455_school1";
+$mysql_user = "a4451455_jazib";
+$mysql_password = "jazib123"; -->
--- home.php
+++ home.php
@@ -1,4 +1,5 @@
-<div class="col-md-12">	
+
+<div class="col-md-12">
 	<img src="picture/master.jpg" class="img-responsive">
 	<h4>What do you know about School?</h4>
 	<p>Stylized implementation of HTML's <abbr> element for abbreviations and acronyms to show the expanded version on hover. Abbreviations with a title attribute have a light dotted bottom border and a help cursor on hover, providing additional context on hover and to users of assistive technologies.</p>
