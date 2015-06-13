fc2e58a2a06cb5400a7972485c9ae1e897f5b334
Jazib  Bashir <jazibbashir@gmail.com>
2015-06-13 11:54:39 +0500

    subject form changed
    *** entry form converted to bootstrap
    *** entry form conenct with db
    *** entry form validated

--- Subject_Entry.php
+++ Subject_Entry.php
@@ -177,92 +177,71 @@ if($opr=="upd")
 else
 {
 ?>
-
-<div id="top_style">
-        <div id="top_style_text">
-        Subjects Entry
-        </div><!-- end of top_style_text-->
-       <div id="top_style_button"> 
-       		<form method="post">
-            	<a href="?tag=view_subjects" ><input type="button" name="btn_view" title="View Subjects" value="View_Subjects" id="button_view" style="width:120px;"  /></a>
-             
-       		</form>
-       </div><!-- end of top_style_button-->
-</div><!-- end of top_style-->
-
-
-<div id="style_informations">
-	<form method="post">
-    	<div>
-        	<table border="0" cellpadding="5" cellspacing="0">
-        	<tr>
-            	<td>Facuties's Name</td>
-            	<td>
-                	<select name="factxt" id="textbox">
-                    	<option>---- Facuries's Name   ------</option>
+<div class="col-md-10 col-md-offset-1 form-style">
+    <div class="col-md-12 entry-head margin-20b">
+        <h4 class="left">Subjects Entry</h4>
+        <a class="btn btn-primary right" href="?tag=view_subjects">View Subjects</a>
+    </div>
+    <div class="col-md-10 col-md-offset-1">
+        <form role="form" data-toggle="validator" method="post" class="form-horizontal">
+            <div class="row">
+                <div class="form-group">
+                    <label class="control-label col-sm-3">facultie's Name:</label>
+                    <div class="col-sm-8">
+                        <select name="factxt" class="form-control">
                             <?php
-                               $fac_name=mysql_query("SELECT * FROM facuties_tbl");
-							   while($row=mysql_fetch_array($fac_name)){
-								?>
-                        		<option value="<?php echo $row['faculties_id'];?>"> <?php echo $row['faculties_name'];?> </option>
-                                <?php 
-							   }
+                            $fac_name=mysql_query("SELECT * FROM facuties_tbl");
+                            while($row=mysql_fetch_array($fac_name)){
+                                ?>
+                                <option value="<?php echo $row['faculties_id'];?>"> <?php echo $row['faculties_name'];?> </option>
+                            <?php
+                            }
                             ?>
-                    </select>
-                </td>
-            </tr>
-            
-            <tr>
-            	<td>Teacher's Name</td>
-            	<td>
-                	<select name="techtxt" id="textbox">
-                    	<option>---- Teachers's Name   ----</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-3">Teacher's Name:</label>
+                    <div class="col-sm-8">
+                        <select name="techtxt" class="form-control">
                             <?php
-                                $te_name=mysql_query("SELECT * FROM teacher_tbl");
-								while($row=mysql_fetch_array($te_name)){
-								?>
+                            $te_name=mysql_query("SELECT * FROM teacher_tbl");
+                            while($row=mysql_fetch_array($te_name)){
+                                ?>
                                 <option value="<?php echo $row['teacher_id'];?>"> <?php echo $row['f_name'] ; echo " "; echo $row['l_name'];?> </option>
-                                	
-								<?php	
-									}
+
+                            <?php
+                            }
                             ?>
-                    </select>
-                </td>
-            </tr>
-            
-            <tr>
-            	<td>Semester</td>
-            	<td>
-                	<input type="text" name="semestertxt" id="textbox"  />
-                </td>
-            </tr>
-            
-            <tr>
-            	<td>Subjects's name</td>
-                <td>
-                	<input type="text" name="subtxt"  id="textbox" />
-                </td>
-            </tr>
-            
-            <tr>
-            	<td>Note</td>
-                <td>
-                	<textarea name="notetxt" cols="23" rows="3"></textarea>
-                </td>
-            </tr>
-            
-            <tr>
-                <td colspan="2">
-                	<input type="reset" value="Cancel" id="button-in"/>
-                	<input type="submit" name="btn_sub" value="Add Now" id="button-in"  />
-                </td>
-            </tr>
-		</table>
-
-    	</div>
-    </form>
-
-</div><!-- end of style_informatios -->
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="smesterText" class="control-label col-sm-3">Smester:</label>
+                    <div class="col-sm-8">
+                        <input id="txtboxToFilter" type="number" class="form-control" id="smesterText" name="semestertxt"  placeholder="Smester(e.g: 1-8)" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="subjectInput" class="control-label col-sm-3">Subjects's name:</label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="subjectInput" name="subtxt"  placeholder="Subject Name" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputNote" class="control-label col-sm-3">Description:</label>
+                    <div class="col-sm-8">
+                        <textarea class="form-control" id="inputNote" name="notetxt" cols="8" rows="6" required></textarea>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="btn_sub" value="Add Subject" class="btn btn-success col-md-offset-4 col-sm-offset-4 col-xs-offset-2"/>
+                    <input type="reset" value="Cancel" class="btn btn-primary col-md-offset-3 col-sm-offset-3 col-xs-offset-3"/>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
 
 <?php
 }
