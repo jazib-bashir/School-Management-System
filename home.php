5ca2ac09edfe31a3e4e8de27f258254ee0ec33cc
Jazib  Bashir <jazibbashir@gmail.com>
2015-06-21 12:05:17 +0500

    new home wigdet added

--- home.php
+++ home.php
@@ -38,6 +38,32 @@
             </span>
         </div>
     </div>
+    <div class="col-lg-4 col-sm-6 col-xs-12 main-widget">
+        <div class="main-box infographic-box">
+            <i class="fa fa-fax orange-bg"></i>
+            <span class="headline">Faculty</span>
+            <span class="value">
+                <?php
+                $sql_sel=mysql_query("SELECT * FROM facuties_tbl");
+                $count = mysql_num_rows($sql_sel);
+                echo $count;
+                ?>
+            </span>
+        </div>
+    </div>
+    <div class="col-lg-4 col-sm-6 col-xs-12 main-widget">
+        <div class="main-box infographic-box">
+            <i class="fa fa-star yellow-bg"></i>
+            <span class="headline">Subject</span>
+            <span class="value">
+                <?php
+                $sql_sel=mysql_query("SELECT * FROM sub_tbl");
+                $count = mysql_num_rows($sql_sel);
+                echo $count;
+                ?>
+            </span>
+        </div>
+    </div>
     <div class="col-md-12 wrapper">
         <div class="white-color">
             <h4>What do you know about School?</h4>
